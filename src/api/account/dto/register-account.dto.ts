736aeaee6aca7a6606faec4693cc6aaa153645e3
yeom-c <yjc720@gmail.com>
2023-06-08 14:08:47 +0900

    feat: dto validation

--- src/api/account/dto/register-account.dto.ts
+++ src/api/account/dto/register-account.dto.ts
@@ -1,5 +1,15 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
 export class RegisterAccountDto {
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsString()
+  @IsNotEmpty()
   password: string;
 }
